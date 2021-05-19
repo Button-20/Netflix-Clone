760ea444786bdce81811059935e774fde8f45877
Button-20 <jasonaddy51@gmail.com>
2021-05-19 16:01:54 +0000

    Created Homepage

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
+import "./Row.css"
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -11,6 +12,7 @@ function Row({title, fetchUrl}) {
         //If [], run once when the row loads, and don't run again
         async function fetchData() {
             const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
             console.log(request);
             return request;
         }
@@ -25,9 +27,9 @@ function Row({title, fetchUrl}) {
             <div className="row__posters">
                 {/* several row__posters */}
 
-                {movies.map(movie => {
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
-                })}
+                {movies.map((movie) => (
+                    <img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                ))}
             </div>
             {/* container -> posters */}
         </div>
