df68acfba23d5abfe6ce7353f476cdcf9af5a919
Button-20 <jasonaddy51@gmail.com>
2021-05-20 09:44:25 +0000

    Netflix Frontend Clone Complete

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import axios from './axios';
-import "./Row.css"
+import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     //A snippet of code which runs based on a specific condition
     useEffect(() => {
@@ -18,16 +21,38 @@ function Row({title, fetchUrl, isLargeRow}) {
         fetchData();
     }, [fetchUrl])
 
+    const opts = {
+        height: "390",
+        width: "100%",
+        playerVars: {
+            // https
+            autoplay: 1,
+        }
+    }
+
+    const handleClick = (movie) => {
+        if (trailerUrl){
+            setTrailerUrl("");
+        }else{
+            movieTrailer(movie?.name || "")
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search);
+                setTrailerUrl(urlParams.get('v'));
+            })
+            .catch((error) => console.log(error))
+        }
+    }
+
     return (
         <div className="row">
             <h2>{title}</h2>
 
             <div className="row__posters">
-                {/* several row__posters */}
 
                 {movies.map((movie) => (
                     <img 
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${
                             isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -35,7 +60,8 @@ function Row({title, fetchUrl, isLargeRow}) {
                         alt={movie.name}/>
                 ))}
             </div>
-            {/* container -> posters */}
+            
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
         </div>
     )
 }
